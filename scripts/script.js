8d9b649f6cdb74ddec3c5a6c24e81ecec5948a32
Kaleb Link <kaleb.s.link@gmail.com>
2024-06-04 15:18:43 -0700

    added canva stuff, still figring out

---
+++ scripts/script.js
@@ -0,0 +1,34 @@
+"use strict";
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext("2d");
+
+
+const gameBoard = {
+    cols: 10,
+    rows: 10,
+    cellSize: canvas.width / 10,
+};
+
+class Player {
+    constructor() {
+
+    }
+}
+
+function createGrid(cols, rows) {
+    const grid = [];
+    for (let c = 0; c < cols; c++) {
+        grid[c] = [];
+        for (let r = 0; r < rows; r++) {
+            grid[c][r] = 0;
+        }
+    }
+    return grid;
+}
+
+const grid = createGrid(gameBoard.cols, gameBoard.rows);
+
+function drawGrid() {
+    // ctx.beginPath();
+    // ctx.rect();
+}
